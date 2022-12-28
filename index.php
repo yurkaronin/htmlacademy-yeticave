c174fb604906b6373ecffc155fc1b130cc0ab885
RoninsWork <yurkaronin@gmail.com>
2022-12-28 18:03:35 +0300

    Добавил коментарии

--- index.php
+++ index.php
@@ -5,13 +5,21 @@ require_once('data.php');
 require_once('models.php');
 
 // Проверка соединения с БД
+// если соединения нет - показываем код ошибки 
 if (!$connect_db) {
     $error = mysqli_connect_error();
+
+    // а если соединение есть 
 } else {
+    // делаем запрос на получение категорий из БД 
     $sql = "SELECT character_code, name_category FROM categories";
+    // полученный результат сохраняем в переменную 
     $result = mysqli_query($connect_db, $sql);
+    // если данные получены без ошибок 
     if ($result) {
+        // преобразуем полученные данные в двумерный массив 
         $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        // а если получили ошибку, то показываем сообщение об ошибке 
     } else {
         $error = mysqli_error($connect_db);
     }
--- init.php
+++ init.php
@@ -2,8 +2,8 @@
 $is_auth = rand(0, 1);
 $user_name = 'Рональд Курочкин';
 
+// соединение с БД 
 $connect_db = mysqli_connect("localhost", "root", "", "yeticave");
-
 mysqli_set_charset($connect_db, "utf8");
 
 ?>
