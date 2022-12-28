4e46efd6d7f98034f499f0e5ee642c6aaf4aaa95
Yuriy Savchenko <47382042+yurkaronin@users.noreply.github.com>
2022-12-28 21:31:41 +0300

    Дополнил коментарии

--- index.php
+++ index.php
@@ -8,27 +8,28 @@ require_once('models.php');
 // если соединения нет - показываем код ошибки 
 if (!$connect_db) {
     $error = mysqli_connect_error();
-
-    // а если соединение есть 
 } else {
-    // делаем запрос на получение категорий из БД 
+    // если нет ошибок > делаем запрос на получение категорий из БД 
     $sql = "SELECT character_code, name_category FROM categories";
-    // полученный результат сохраняем в переменную 
+    // полученный результат сохраняем в переменную
     $result = mysqli_query($connect_db, $sql);
-    // если данные получены без ошибок 
+    // если данные получены без ошибок
     if ($result) {
-        // преобразуем полученные данные в двумерный массив 
+        // преобразуем полученные данные в двумерный массив
         $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        // а если получили ошибку, то показываем сообщение об ошибке 
+        // а если получили ошибку, то показываем сообщение об ошибке
     } else {
         $error = mysqli_error($connect_db);
     }
 };
 
+// сохраняем в переменную результат работы функции 
+// которая (Подключает шаблон, передает туда данные и возвращает итоговый HTML контент)
 $page_content = include_template('main.php', [
     'product_categories' => $product_categories,
     'product_items' => $product_items,
 ]);
+
 
 $layout_content = include_template('layout.php', [
     'content' => $page_content,
